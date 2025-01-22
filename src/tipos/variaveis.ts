a68c180d08f112c1477966de8fdc7d78511b902c
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-01-21 22:31:52 -0300

    aula de tipos básicos do typescript

---
+++ src/tipos/variaveis.ts
@@ -0,0 +1,14 @@
+const nome: string = 'João';
+console.log(nome)
+
+let estaChovendo: boolean = true;
+console.log(estaChovendo)
+
+const idade: number = 27;
+console.log(idade)
+
+let variavel: any = 4
+variavel = 'Olá'
+variavel = true
+variavel = {nome: 'João'}
+console.log(variavel)
