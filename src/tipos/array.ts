a8687d20dfd5782fe485831c476a8ff9a29f3a8b
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-01-21 22:42:02 -0300

    aula de arrays

---
+++ src/tipos/array.ts
@@ -0,0 +1,10 @@
+let hobbies: string[] = ['Cozinhar', 'Praticar esportes']
+console.log(hobbies[0])
+console.log(typeof hobbies)
+
+// hobbies = [100]
+console.log(hobbies)
+
+let endereco: [string, number] = ['Rua A', 99]
+endereco = ['Rua B', 100]
+console.log(endereco)
